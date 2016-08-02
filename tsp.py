9a35f634dfaeb7031d442b55ae07062bbfef6dfc
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-08-02 03:59:13 -0500

    Added initial representation for points in the plane.

---
+++ tsp.py
@@ -0,0 +1,8 @@
+from point import Point
+
+# Set of points in the plane to be used for testing.
+points = set()
+points.add(Point(0,0))
+points.add(Point(0,1))
+points.add(Point(1,2))
+points.add(Point(2,2))
