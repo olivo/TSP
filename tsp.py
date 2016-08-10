aabaef72b92571465def76087350471e7d5991aa
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-08-10 00:33:43 -0500

    Implemented TSP route computation using the nearest neighbor.

--- tsp.py
+++ tsp.py
@@ -4,6 +4,19 @@ import sys
 def nearest_neighbor(points, current_point):
     return min(points, key = lambda x: Point.distance(x, current_point) if x != current_point else sys.maxint)
 
+def compute_nearest_neighbor_tsp_route(points):
+    current_point = points[0]
+    route = [current_point]
+    points.remove(current_point)
+
+    while points:
+        neighbor = nearest_neighbor(points, current_point)
+        route.append(neighbor)
+        print "Neighbor:", Point.str(neighbor)
+        points.remove(neighbor)
+
+    return route
+
 # List of points in the plane to be used for testing.
 points = list()
 points.append(Point(0,0))
@@ -11,8 +24,8 @@ points.append(Point(0,1))
 points.append(Point(1,2))
 points.append(Point(2,2))
 
-start_point = points[0]
+tsp_route = compute_nearest_neighbor_tsp_route(points)
 
-nearest_neighbor = nearest_neighbor(points, start_point)
-
-print "The nearest neighbor is:", Point.str(nearest_neighbor)
+print "The TSP route using the nearest neighbor is:"
+for point in tsp_route:
+    print Point.str(point), " "
